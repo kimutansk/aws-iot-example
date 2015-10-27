5246bb88a38ade0a559d9c5af6b7c19917ccf0cd
Kimura Sotaro <rfbringer@gmail.com>
2015-10-27 10:53:44 +0900

    Add MQTT Publisher

--- src/main/scala/com/github/kimutansk/awsiot/SocketFactoryGenerator.scala
+++ src/main/scala/com/github/kimutansk/awsiot/SocketFactoryGenerator.scala
@@ -1,13 +1,15 @@
 package com.github.kimutansk.awsiot
 
-import java.io.{ByteArrayInputStream, InputStreamReader}
-import java.nio.file.{Paths, Files}
+import java.io.{ByteArrayInputStream, InputStream, InputStreamReader}
+import java.nio.file.{Files, Paths}
+import java.security.cert.{CertificateFactory, X509Certificate}
 import java.security.{KeyPair, KeyStore, Security}
-import java.security.cert.X509Certificate
-import javax.net.ssl.{SSLContext, KeyManagerFactory, TrustManagerFactory, SSLSocketFactory}
+import javax.net.ssl.{KeyManagerFactory, SSLContext, SSLSocketFactory, TrustManagerFactory}
 
+import org.bouncycastle.cert.X509CertificateHolder
 import org.bouncycastle.jce.provider.BouncyCastleProvider
-import org.bouncycastle.openssl.PEMParser
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter
+import org.bouncycastle.openssl.{PEMKeyPair, PEMParser}
 
 /** Factory for [[javax.net.ssl.SSLSocketFactory]] instances. */
 object SocketFactoryGenerator {
@@ -25,23 +27,26 @@ object SocketFactoryGenerator {
 
     // load Root CA certificate
     val rootCaParser:PEMParser  = new PEMParser(new InputStreamReader(new ByteArrayInputStream(Files.readAllBytes(Paths.get(rootCaFilePath)))))
-    val rootCaCert:X509Certificate = rootCaParser.readObject().asInstanceOf[X509Certificate]
+    val rootCaCertHolder:X509CertificateHolder = rootCaParser.readObject().asInstanceOf[X509CertificateHolder]
+    val rootCaCert:X509Certificate = convertToJavaCertificate(rootCaCertHolder)
     rootCaParser.close()
 
     // load Server certificate
     val certParser:PEMParser = new PEMParser(new InputStreamReader(new ByteArrayInputStream(Files.readAllBytes(Paths.get(certFilePath)))))
-    val serverCert:X509Certificate = certParser.readObject.asInstanceOf[X509Certificate]
+    val serverCertHolder:X509CertificateHolder = certParser.readObject.asInstanceOf[X509CertificateHolder]
+    val serverCert:X509Certificate = convertToJavaCertificate(serverCertHolder)
     certParser.close()
 
     // load Private Key
     val keyParser:PEMParser = new PEMParser(new InputStreamReader(new ByteArrayInputStream(Files.readAllBytes(Paths.get(keyFilePath)))))
-    val keyPair:KeyPair = keyParser.readObject.asInstanceOf[KeyPair]
+    val pemKeyPair:PEMKeyPair = keyParser.readObject.asInstanceOf[PEMKeyPair]
+    val keyPair:KeyPair = new JcaPEMKeyConverter().getKeyPair(pemKeyPair)
     keyParser.close()
 
     // Root CA certificate is used to authenticate server
     val rootCAKeyStore:KeyStore = KeyStore.getInstance(KeyStore.getDefaultType())
     rootCAKeyStore.load(null, null)
-    rootCAKeyStore.setCertificateEntry("ca-certificate", rootCaCert);
+    rootCAKeyStore.setCertificateEntry("ca-certificate", convertToJavaCertificate(rootCaCertHolder))
     val tmf:TrustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
     tmf.init(rootCAKeyStore);
 
@@ -58,5 +63,13 @@ object SocketFactoryGenerator {
     context.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null)
 
     context.getSocketFactory()
+  }
+
+
+  def convertToJavaCertificate(certificateHolder:X509CertificateHolder):X509Certificate = {
+     val is:InputStream = new ByteArrayInputStream(certificateHolder.toASN1Structure.getEncoded);
+    try {
+      CertificateFactory.getInstance("X.509").generateCertificate(is).asInstanceOf[X509Certificate]
+    } finally is.close()
   }
 }
